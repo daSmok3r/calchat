7f7204df1d5c9ba0f1e2b9a9313495bd75c214ad
Tobias Hansen <tobias.hansen@smail.inf.h-brs.de>
2016-06-23 17:07:06 +0200

    term.js complete

--- components/term-chat/term.js
+++ components/term-chat/term.js
@@ -3,15 +3,58 @@ ccm.component({
 
     config:{
         key:'test',
-        store: [ccm.store,{url:'../../dataset/data.json',store:'term'}],
+        store: [ccm.store,{local:'../../dataset/data.json',store:'term'}],
         user:  [ ccm.instance, 'https://kaul.inf.h-brs.de/ccm/components/user2.js' ]
     },
 
-    Instance: function(){
+    Instance: function () {
 
-        var self =this;
-        self.init = function(callback){
+        var self = this;
 
-            self.store.onChange = function(){self.render();};
-            callblack();
+        self.init = function ( callback ) {
+
+            self.store.onChange = function () { self.render(); };
+
+            callback();
+
         };
+
+        self.render = function ( callback ) {
+
+            var element = ccm.helper.element( self );
+
+            self.store.get( self.key, function ( dataset ) {
+
+                if ( dataset === null )
+                    self.store.set( { key: self.key, calculation: [] }, proceed );
+                else
+                    proceed( dataset );
+
+                function proceed( dataset ) {
+
+                    element.html( ccm.helper.html( self.html.get( 'main' ) ) );
+
+                    var calculation_div = ccm.helper.find( self, '.calculation' );
+
+                    for ( var i = 0; i < dataset.calculation.length; i++ ) {
+
+                        var calculation = dataset.calculation[ i ];
+
+                        calculation_div.append( ccm.helper.html( self.html.get( 'calculation' ), {
+
+                            term: ccm.helper.val( calculation.term ),
+                            name: ccm.helper.val( calculation.name )
+
+                        } ) );
+
+                    }
+                    
+                }
+
+            } );
+
+        };
+
+    }
+
+} );
